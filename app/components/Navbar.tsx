ade5321bf6ed3c245ef601b4a6de3aca4e42cbf1
Anjum109 <aunjumtuli109@gmail.com>
2025-11-27 21:10:30 +0600

    title

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,12 +2,17 @@ import { advent_pro, monoton } from '@/font/font'
 import { CgProfile } from "react-icons/cg";
 import { FaSearch } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
+import logo from '../assets/logo/favicon.png'
+import Image from 'next/image';
 
 export default function Navbar() {
     return (
         <div className='py-5 px-20 text-white'>
             <div className='flex justify-between  items-center w-full'>
-                <h1 className='text-3xl'><span className={monoton.className}>Flavor Dash</span></h1>
+                <div className='flex items-center gap-2'>
+                    <Image src={logo} alt='logo' className='z-10 w-[40px]' />
+                    <h1 className='text-3xl'><span className={monoton.className}>Flavor Dash</span></h1>
+                </div>
                 <div className={advent_pro.className}>
                     <ul className='flex gap-2 items-center text-[22px] underline'>
                         <li className=' cursor-pointer hover:text-amber-300 duration-1000'>Home</li>
